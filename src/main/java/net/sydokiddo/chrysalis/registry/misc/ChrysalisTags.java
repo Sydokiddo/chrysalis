925c32f36ed519e956b899d523abc0a7f931d691
Sydney <94016034+Sydokiddo@users.noreply.github.com>
2023-07-22 20:28:58 -0700

    unbreakable_blocks Tag

--- src/main/java/net/sydokiddo/chrysalis/registry/misc/ChrysalisTags.java
+++ src/main/java/net/sydokiddo/chrysalis/registry/misc/ChrysalisTags.java
@@ -89,6 +89,7 @@ public class ChrysalisTags {
     public static final TagKey<Block> LANTERNS = TagKey.create(Registries.BLOCK, new ResourceLocation(Chrysalis.MOD_ID, "lanterns"));
     public static final TagKey<Block> COPPER_BLOCKS = TagKey.create(Registries.BLOCK, new ResourceLocation(Chrysalis.MOD_ID, "copper_blocks"));
     public static final TagKey<Block> ALLOWS_BEACON_BEAM_PASSTHROUGH = TagKey.create(Registries.BLOCK, new ResourceLocation(Chrysalis.MOD_ID, "allows_beacon_beam_passthrough"));
+    public static final TagKey<Block> UNBREAKABLE_BLOCKS = TagKey.create(Registries.BLOCK, new ResourceLocation(Chrysalis.MOD_ID, "unbreakable_blocks"));
 
     /**
      * Entity Tags
