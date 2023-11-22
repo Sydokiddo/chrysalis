3ef3e6f6d0fc70c54005d8e34224b25cde190c74
Sydney <94016034+Sydokiddo@users.noreply.github.com>
2023-11-21 22:19:00 -0800

    Converted DateHelper to a class

--- src/main/java/net/sydokiddo/chrysalis/misc/util/DateHelper.java
+++ src/main/java/net/sydokiddo/chrysalis/misc/util/DateHelper.java
@@ -4,29 +4,29 @@ import java.time.LocalDate;
 import java.time.temporal.ChronoField;
 
 @SuppressWarnings("unused")
-public interface DateHelper {
+public class DateHelper {
 
     /**
      * Gets specific dates for various methods that only trigger on said specific date.
      **/
 
-    LocalDate localDate = LocalDate.now();
-    int getMonth = localDate.get(ChronoField.MONTH_OF_YEAR);
-    int getDay = localDate.get(ChronoField.DAY_OF_MONTH);
+    public static final LocalDate localDate = LocalDate.now();
+    public static final int getMonth = localDate.get(ChronoField.MONTH_OF_YEAR);
+    public static final int getDay = localDate.get(ChronoField.DAY_OF_MONTH);
 
-    default boolean isCustomDate(int month, int day) {
+    public static boolean isCustomDate(int month, int day) {
         return getMonth == month && getDay == day;
     }
 
-    default boolean isHalloween() {
+    public static boolean isHalloween() {
         return getMonth == 10 && getDay == 31;
     }
 
-    default boolean isChristmas() {
+    public static boolean isChristmas() {
         return getMonth == 12 && getDay == 25;
     }
 
-    default boolean isAprilFools() {
+    public static boolean isAprilFools() {
         return getMonth == 4 && getDay == 1;
     }
 }
