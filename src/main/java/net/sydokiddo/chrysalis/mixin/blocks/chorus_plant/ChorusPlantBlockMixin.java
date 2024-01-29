9565b9c017d19913a535c243745339270350af80
Sydney <94016034+Sydokiddo@users.noreply.github.com>
2024-01-28 22:24:46 -0800

    Fixed a critical issue involving ticking in other dimensions

--- src/main/java/net/sydokiddo/chrysalis/mixin/blocks/chorus_plant/ChorusPlantBlockMixin.java
+++ src/main/java/net/sydokiddo/chrysalis/mixin/blocks/chorus_plant/ChorusPlantBlockMixin.java
@@ -1,37 +1,89 @@
 package net.sydokiddo.chrysalis.mixin.blocks.chorus_plant;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.ChorusPlantBlock;
+import net.minecraft.world.level.block.PipeBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.sydokiddo.chrysalis.registry.misc.ChrysalisTags;
+import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ChorusPlantBlock.class)
-public class ChorusPlantBlockMixin {
+public abstract class ChorusPlantBlockMixin extends PipeBlock {
+
+    private ChorusPlantBlockMixin(float shapeByIndex, Properties properties) {
+        super(shapeByIndex, properties);
+    }
 
     /**
      * Chorus Plants are now placeable on any blocks in the chorus_plant_can_grow_on tag.
      **/
 
-    @Redirect(method = "getStateWithConnections", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;is(Lnet/minecraft/world/level/block/Block;)Z", ordinal = 2))
-    private static boolean chrysalis$chorusPlantGetStateWithConnections(BlockState blockState, Block block) {
-        return blockState.is(ChrysalisTags.CHORUS_PLANT_CAN_GROW_ON);
+    @Inject(method = "getStateWithConnections", at = @At("HEAD"), cancellable = true)
+    private static void chrysalis$chorusPlantGetStateWithConnections(BlockGetter blockGetter, BlockPos blockPos, BlockState blockState, CallbackInfoReturnable<BlockState> cir) {
+
+        BlockState belowState = blockGetter.getBlockState(blockPos.below());
+        BlockState aboveState = blockGetter.getBlockState(blockPos.above());
+
+        BlockState northState = blockGetter.getBlockState(blockPos.north());
+        BlockState eastState = blockGetter.getBlockState(blockPos.east());
+        BlockState southState = blockGetter.getBlockState(blockPos.south());
+        BlockState westState = blockGetter.getBlockState(blockPos.west());
+
+        Block block = blockState.getBlock();
+        Block chorusFlower = Blocks.CHORUS_FLOWER;
+
+        cir.setReturnValue(blockState.trySetValue(DOWN, belowState.is(block) || belowState.is(chorusFlower) || belowState.is(ChrysalisTags.CHORUS_PLANT_CAN_GROW_ON))
+        .trySetValue(UP, aboveState.is(block) || aboveState.is(chorusFlower))
+        .trySetValue(NORTH, northState.is(block) || northState.is(chorusFlower))
+        .trySetValue(EAST, eastState.is(block) || eastState.is(chorusFlower))
+        .trySetValue(SOUTH, southState.is(block) || southState.is(chorusFlower))
+        .trySetValue(WEST, westState.is(block) || westState.is(chorusFlower)));
     }
 
-    @Redirect(method = "updateShape", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;is(Lnet/minecraft/world/level/block/Block;)Z", ordinal = 2))
-    public boolean chrysalis$chorusPlantUpdateShape(BlockState blockState, Block block) {
-        return blockState.is(ChrysalisTags.CHORUS_PLANT_CAN_GROW_ON);
+    @Inject(method = "updateShape", at = @At("RETURN"), cancellable = true)
+    private void chrysalis$chorusPlantUpdateShape(BlockState blockState, Direction direction, BlockState adjacentState, LevelAccessor levelAccessor, BlockPos blockPos, BlockPos adjacentPos, CallbackInfoReturnable<BlockState> cir) {
+        if (blockState.canSurvive(levelAccessor, blockPos)) {
+            boolean canGrowFrom = adjacentState.is(this) || adjacentState.is(Blocks.CHORUS_FLOWER) || direction == Direction.DOWN && adjacentState.is(ChrysalisTags.CHORUS_PLANT_CAN_GROW_ON);
+            cir.setReturnValue(blockState.setValue(PROPERTY_BY_DIRECTION.get(direction), canGrowFrom));
+        }
     }
 
-    @Redirect(method = "canSurvive", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;is(Lnet/minecraft/world/level/block/Block;)Z", ordinal = 1))
-    private boolean chrysalis$chorusPlantCanSurvive1(BlockState blockState, Block block) {
-        return blockState.is(ChrysalisTags.CHORUS_PLANT_CAN_GROW_ON);
-    }
+    /**
+     * @author Sydokiddo
+     * @reason Overrides the base canSurvive method and allows for Chorus Plants to grow on the chorus_plant_can_grow_on tag
+     */
 
-    @Redirect(method = "canSurvive", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;is(Lnet/minecraft/world/level/block/Block;)Z", ordinal = 4))
-    private boolean chrysalis$chorusPlantCanSurvive2(BlockState blockState, Block block) {
-        return blockState.is(ChrysalisTags.CHORUS_PLANT_CAN_GROW_ON);
+    @SuppressWarnings("deprecation")
+    @Overwrite
+    public boolean canSurvive(@NotNull BlockState blockState, LevelReader levelReader, BlockPos blockPos) {
+
+        BlockState belowBlockState = levelReader.getBlockState(blockPos.below());
+
+        for (Direction direction : Direction.Plane.HORIZONTAL) {
+
+            BlockPos relativeBlockPos = blockPos.relative(direction);
+            if (!levelReader.getBlockState(relativeBlockPos).is(this)) continue;
+
+            if (!levelReader.getBlockState(blockPos.above()).isAir() && !belowBlockState.isAir()) {
+                return false;
+            }
+
+            BlockState belowRelative = levelReader.getBlockState(relativeBlockPos.below());
+            if (!belowRelative.is(this) && !belowRelative.is(ChrysalisTags.CHORUS_PLANT_CAN_GROW_ON)) continue;
+
+            return true;
+        }
+        return belowBlockState.is(this) || belowBlockState.is(ChrysalisTags.CHORUS_PLANT_CAN_GROW_ON);
     }
 }
--- src/main/java/net/sydokiddo/chrysalis/mixin/misc/SnowFixMixin.java
+++ src/main/java/net/sydokiddo/chrysalis/mixin/misc/SnowFixMixin.java
@@ -1,8 +1,8 @@
 package net.sydokiddo.chrysalis.mixin.misc;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.chunk.LevelChunk;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
@@ -29,8 +29,8 @@ public abstract class SnowFixMixin {
      * Prevents a bug where snow can fall in the Nether and End if the Y-level can go high enough.
      **/
 
-    @Inject(method = "tickChunk", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerLevel;tickPrecipitation(Lnet/minecraft/core/BlockPos;)V"), cancellable = true)
-    private void chrysalis$preventSnowInNetherAndEnd(LevelChunk levelChunk, int i, CallbackInfo info) {
+    @Inject(method = "tickPrecipitation", at = @At(value = "HEAD"), cancellable = true)
+    private void chrysalis$preventSnowInNetherAndEnd(BlockPos blockPos, CallbackInfo info) {
         if (isNether() || isEnd()) {
             info.cancel();
         }
