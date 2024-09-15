1ac95e31343df7d3f0bfa5a86d3d9f5b44ead85e
Sydney <94016034+Sydokiddo@users.noreply.github.com>
2024-09-14 23:50:02 -0700

    Changed back to duplicate_allay criteria trigger

--- src/main/java/net/sydokiddo/chrysalis/registry/misc/ChrysalisCriteriaTriggers.java
+++ src/main/java/net/sydokiddo/chrysalis/registry/misc/ChrysalisCriteriaTriggers.java
@@ -7,7 +7,7 @@ public class ChrysalisCriteriaTriggers extends CriteriaTriggers {
 
     // Criteria Triggers
 
-    public static final PlayerTrigger DUPLICATE_MOB = ChrysalisCriteriaTriggers.register("chrysalis:duplicate_mob", new PlayerTrigger());
+    public static final PlayerTrigger DUPLICATE_ALLAY = ChrysalisCriteriaTriggers.register("chrysalis:duplicate_allay", new PlayerTrigger());
     public static final PlayerTrigger CURE_POISON_WITH_HONEY = ChrysalisCriteriaTriggers.register("chrysalis:cure_poison_with_honey", new PlayerTrigger());
     public static final PlayerTrigger USE_RIPTIDE_TRIDENT = ChrysalisCriteriaTriggers.register("chrysalis:use_riptide_trident", new PlayerTrigger());
 
