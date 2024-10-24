7b68a1b6144ecf2fc0c5eb9fdddff684d3b333a7
Sydney <94016034+Sydokiddo@users.noreply.github.com>
2024-10-24 15:19:16 -0700

    More entity render state things

--- src/main/java/net/sydokiddo/chrysalis/misc/util/entities/ChrysalisEntityRenderState.java
+++ src/main/java/net/sydokiddo/chrysalis/misc/util/entities/ChrysalisEntityRenderState.java
@@ -2,11 +2,13 @@ package net.sydokiddo.chrysalis.misc.util.entities;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.client.renderer.entity.state.LivingEntityRenderState;
+import net.minecraft.client.renderer.entity.state.EntityRenderState;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 
 @Environment(EnvType.CLIENT)
-public class ChrysalisEntityRenderState extends LivingEntityRenderState {
+public class ChrysalisEntityRenderState extends EntityRenderState {
 
+    public static Entity entity;
     public static LivingEntity livingEntity;
 }
---
+++ src/main/java/net/sydokiddo/chrysalis/mixin/misc/EntityRendererMixin.java
@@ -0,0 +1,69 @@
+package net.sydokiddo.chrysalis.mixin.misc;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.entity.EntityRenderer;
+import net.minecraft.client.renderer.entity.LivingEntityRenderer;
+import net.minecraft.client.renderer.entity.layers.HumanoidArmorLayer;
+import net.minecraft.client.renderer.entity.layers.WingsLayer;
+import net.minecraft.client.renderer.entity.state.EntityRenderState;
+import net.minecraft.client.renderer.entity.state.HumanoidRenderState;
+import net.minecraft.client.renderer.entity.state.LivingEntityRenderState;
+import net.minecraft.core.Holder;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.sydokiddo.chrysalis.misc.util.entities.ChrysalisEntityRenderState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import java.util.Objects;
+
+@Environment(EnvType.CLIENT)
+@Mixin(EntityRenderer.class)
+public abstract class EntityRendererMixin {
+
+    @Inject(method = "extractRenderState", at = @At("HEAD"))
+    private void chrysalis$addEntityRenderStates(Entity entity, EntityRenderState entityRenderState, float tickCount, CallbackInfo info) {
+        ChrysalisEntityRenderState.entity = entity;
+    }
+
+    @SuppressWarnings("unused")
+    @Environment(EnvType.CLIENT)
+    @Mixin(LivingEntityRenderer.class)
+    public static abstract class LivingEntityRendererMixin {
+
+        @Inject(method = "extractRenderState(Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/client/renderer/entity/state/LivingEntityRenderState;F)V", at = @At("HEAD"))
+        private void chrysalis$addLivingEntityRenderStates(LivingEntity livingEntity, LivingEntityRenderState livingEntityRenderState, float tickCount, CallbackInfo info) {
+            ChrysalisEntityRenderState.livingEntity = livingEntity;
+        }
+    }
+
+    @SuppressWarnings("unused")
+    @Environment(EnvType.CLIENT)
+    @Mixin(HumanoidArmorLayer.class)
+    public static class HumanoidArmorLayerMixin {
+
+        @Inject(method = "render(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;ILnet/minecraft/client/renderer/entity/state/HumanoidRenderState;FF)V", at = @At("HEAD"), cancellable = true)
+        private void chrysalis$hideRenderedArmor(PoseStack poseStack, MultiBufferSource multiBufferSource, int color, HumanoidRenderState humanoidRenderState, float float1, float float2, CallbackInfo info) {
+            Holder<MobEffect> invisibility = MobEffects.INVISIBILITY;
+            if (ChrysalisEntityRenderState.livingEntity.hasEffect(invisibility) && Objects.requireNonNull(ChrysalisEntityRenderState.livingEntity.getEffect(invisibility)).getAmplifier() > 0) info.cancel();
+        }
+    }
+
+    @SuppressWarnings("unused")
+    @Environment(EnvType.CLIENT)
+    @Mixin(WingsLayer.class)
+    public static class WingsLayerMixin {
+
+        @Inject(method = "render(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;ILnet/minecraft/client/renderer/entity/state/HumanoidRenderState;FF)V", at = @At("HEAD"), cancellable = true)
+        private void chrysalis$hideRenderedElytra(PoseStack poseStack, MultiBufferSource multiBufferSource, int color, HumanoidRenderState humanoidRenderState, float float1, float float2, CallbackInfo info) {
+            Holder<MobEffect> invisibility = MobEffects.INVISIBILITY;
+            if (ChrysalisEntityRenderState.livingEntity.hasEffect(invisibility) && Objects.requireNonNull(ChrysalisEntityRenderState.livingEntity.getEffect(invisibility)).getAmplifier() > 0) info.cancel();
+        }
+    }
+}
