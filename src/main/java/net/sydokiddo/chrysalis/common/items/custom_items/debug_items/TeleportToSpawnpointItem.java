6f86d94f4ab8ddbeb3b9f01e3a5782b7cc358a1e
Sydney <94016034+Sydokiddo@users.noreply.github.com>
2025-03-29 19:03:51 -0700

    Teleport to spawnpoint item can now work across dimensions

--- src/main/java/net/sydokiddo/chrysalis/common/items/custom_items/debug_items/TeleportToSpawnpointItem.java
+++ src/main/java/net/sydokiddo/chrysalis/common/items/custom_items/debug_items/TeleportToSpawnpointItem.java
@@ -1,24 +1,28 @@
 package net.sydokiddo.chrysalis.common.items.custom_items.debug_items;
 
 import net.minecraft.ChatFormatting;
+import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.stats.Stats;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.Relative;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.gameevent.GameEvent;
+import net.minecraft.world.level.portal.TeleportTransition;
+import net.minecraft.world.phys.Vec3;
 import net.sydokiddo.chrysalis.util.helpers.ItemHelper;
 import net.sydokiddo.chrysalis.common.items.custom_items.debug_items.base_classes.DebugUtilityItem;
 import net.sydokiddo.chrysalis.common.misc.CSoundEvents;
 import org.jetbrains.annotations.NotNull;
 import java.util.List;
-import java.util.Objects;
 
 public class TeleportToSpawnpointItem extends DebugUtilityItem {
 
@@ -50,22 +54,35 @@ public class TeleportToSpawnpointItem extends DebugUtilityItem {
             serverPlayer.gameEvent(GameEvent.ITEM_INTERACT_FINISH);
             serverPlayer.awardStat(Stats.ITEM_USED.get(this));
 
-            if (serverPlayer.getRespawnPosition() != null) {
+            if (serverPlayer.getServer() != null && serverPlayer.getRespawnPosition() != null) {
 
-                serverPlayer.teleportTo(Objects.requireNonNull(serverPlayer.getRespawnPosition()).getX(), serverPlayer.getRespawnPosition().getY() + 1, serverPlayer.getRespawnPosition().getZ());
-                level.gameEvent(GameEvent.TELEPORT, serverPlayer.position(), GameEvent.Context.of(serverPlayer));
-                level.broadcastEntityEvent(serverPlayer, (byte) 46); // Teleporting Particles
+                ServerLevel respawnDimension = serverPlayer.getServer().getLevel(serverPlayer.getRespawnDimension());
+                BlockPos respawnPos = serverPlayer.getRespawnPosition();
 
-                serverPlayer.playNotifySound(SoundEvents.PLAYER_TELEPORT, SoundSource.PLAYERS, 1.0F, 1.0F);
-                DebugUtilityItem.sendFeedbackMessage(true, serverPlayer, Component.translatable("gui.chrysalis.teleport_to_spawnpoint.message.success"));
-                serverPlayer.getCooldowns().addCooldown(itemStack, 60);
+                if (respawnDimension != null) {
+
+                    serverPlayer.teleport(new TeleportTransition(respawnDimension, new Vec3(respawnPos.getX(), respawnPos.getY() + 1, respawnPos.getZ()), serverPlayer.getDeltaMovement(), serverPlayer.getYRot(), serverPlayer.getXRot(), Relative.union(Relative.DELTA, Relative.ROTATION), TeleportTransition.DO_NOTHING));
+                    level.gameEvent(GameEvent.TELEPORT, serverPlayer.position(), GameEvent.Context.of(serverPlayer));
+                    level.broadcastEntityEvent(serverPlayer, (byte) 46); // Teleporting Particles
+
+                    serverPlayer.playNotifySound(SoundEvents.PLAYER_TELEPORT, SoundSource.PLAYERS, 1.0F, 1.0F);
+                    DebugUtilityItem.sendFeedbackMessage(true, serverPlayer, Component.translatable("gui.chrysalis.teleport_to_spawnpoint.message.success"));
+                    serverPlayer.getCooldowns().addCooldown(itemStack, 60);
+
+                } else {
+                    this.fail(serverPlayer);
+                }
 
             } else {
-                serverPlayer.playNotifySound(CSoundEvents.TELEPORT_TO_SPAWNPOINT_FAIL.get(), SoundSource.PLAYERS, 1.0F, 1.0F);
-                DebugUtilityItem.sendFeedbackMessage(false, serverPlayer, Component.translatable("gui.chrysalis.teleport_to_spawnpoint.message.fail").withStyle(ChatFormatting.RED));
+                this.fail(serverPlayer);
             }
         }
 
         return InteractionResult.SUCCESS.heldItemTransformedTo(itemStack);
+    }
+
+    private void fail(ServerPlayer serverPlayer) {
+        serverPlayer.playNotifySound(CSoundEvents.TELEPORT_TO_SPAWNPOINT_FAIL.get(), SoundSource.PLAYERS, 1.0F, 1.0F);
+        DebugUtilityItem.sendFeedbackMessage(false, serverPlayer, Component.translatable("gui.chrysalis.teleport_to_spawnpoint.message.fail").withStyle(ChatFormatting.RED));
     }
 }
