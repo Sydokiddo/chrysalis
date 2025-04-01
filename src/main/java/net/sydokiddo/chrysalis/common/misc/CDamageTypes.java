77ae7b3df4aa4ce2834e17e53e55e04dac467ed7
Sydney <94016034+Sydokiddo@users.noreply.github.com>
2025-04-01 01:40:24 -0700

    Kill wands now bypass invulnerability frames

--- src/main/java/net/sydokiddo/chrysalis/common/misc/CDamageTypes.java
+++ src/main/java/net/sydokiddo/chrysalis/common/misc/CDamageTypes.java
@@ -12,7 +12,11 @@ public class CDamageTypes {
     @SuppressWarnings("unused")
     public static final ResourceKey<DamageType>
         MOB_ATTACK_BYPASSES_ARMOR = registerDamageType("mob_attack_bypasses_armor"),
+        MOB_ATTACK_BYPASSES_COOLDOWN = registerDamageType("mob_attack_bypasses_cooldown"),
+        MOB_ATTACK_BYPASSES_EFFECTS = registerDamageType("mob_attack_bypasses_effects"),
         MOB_ATTACK_BYPASSES_ENCHANTMENTS = registerDamageType("mob_attack_bypasses_enchantments"),
+        MOB_ATTACK_BYPASSES_INVULNERABILITY = registerDamageType("mob_attack_bypasses_invulnerability"),
+        MOB_ATTACK_BYPASSES_RESISTANCE = registerDamageType("mob_attack_bypasses_resistance"),
         MOB_ATTACK_BYPASSES_SHIELD = registerDamageType("mob_attack_bypasses_shield"),
         MOB_ATTACK_NO_KNOCKBACK = registerDamageType("mob_attack_no_knockback"),
         KILL_WAND = registerDamageType("kill_wand")
