5e76a179c75eaf30904aee0fcc3ffc62345319e9
Sydney <94016034+Sydokiddo@users.noreply.github.com>
2025-04-02 21:19:08 -0700

    Test right click item tweak

--- src/main/java/net/sydokiddo/chrysalis/common/items/custom_items/examples_and_testing/TestRightClickItem.java
+++ src/main/java/net/sydokiddo/chrysalis/common/items/custom_items/examples_and_testing/TestRightClickItem.java
@@ -3,13 +3,13 @@ package net.sydokiddo.chrysalis.common.items.custom_items.examples_and_testing;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.level.Level;
 import net.sydokiddo.chrysalis.Chrysalis;
 import net.sydokiddo.chrysalis.common.entities.custom_entities.spawners.entity_spawner.EntitySpawner;
+import net.sydokiddo.chrysalis.common.items.custom_items.CreativeModeDescriptionItem;
 import org.jetbrains.annotations.NotNull;
 
-public class TestRightClickItem extends Item {
+public class TestRightClickItem extends CreativeModeDescriptionItem {
 
     public TestRightClickItem(Properties properties) {
         super(properties);
