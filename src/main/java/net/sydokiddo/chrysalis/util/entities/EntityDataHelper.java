9a9eeff2267d8a759ee981ddc000c0d5afa66fbd
Sydney <94016034+Sydokiddo@users.noreply.github.com>
2025-04-19 23:31:51 -0700

    dropFromLootTable method

--- src/main/java/net/sydokiddo/chrysalis/util/entities/EntityDataHelper.java
+++ src/main/java/net/sydokiddo/chrysalis/util/entities/EntityDataHelper.java
@@ -14,6 +14,7 @@ import net.minecraft.world.entity.ai.memory.MemoryModuleType;
 import net.minecraft.world.entity.animal.allay.Allay;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.GameRules;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.storage.loot.LootParams;
 import net.minecraft.world.level.storage.loot.LootTable;
@@ -166,6 +167,11 @@ public class EntityDataHelper {
         if (!(entity.level() instanceof ServerLevel serverLevel)) return List.of();
         LootParams lootParams = new LootParams.Builder(serverLevel).withParameter(LootContextParams.ORIGIN, entity.position()).withParameter(LootContextParams.THIS_ENTITY, entity).create(LootContextParamSets.GIFT);
         return lootTable.getRandomItems(lootParams);
+    }
+
+    public static void dropFromLootTable(Entity entity, ResourceKey<LootTable> lootTableKey) {
+        List<ItemStack> lootTable = EntityDataHelper.getNonLivingEntityLootTable(entity, lootTableKey);
+        if (entity.level() instanceof ServerLevel serverLevel && serverLevel.getGameRules().getBoolean(GameRules.RULE_DOMOBLOOT)) for (ItemStack itemStack : lootTable) entity.spawnAtLocation(serverLevel, itemStack);
     }
 
     // endregion
