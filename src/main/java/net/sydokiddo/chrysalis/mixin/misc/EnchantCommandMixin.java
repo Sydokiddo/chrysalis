630edd50a290583847e0c66f859c4fbf6f94aad7
Sydney <94016034+Sydokiddo@users.noreply.github.com>
2025-05-11 19:32:44 -0700

    /enchant command 'max' parameter

--- src/main/java/net/sydokiddo/chrysalis/mixin/misc/EnchantCommandMixin.java
+++ src/main/java/net/sydokiddo/chrysalis/mixin/misc/EnchantCommandMixin.java
@@ -1,9 +1,16 @@
 package net.sydokiddo.chrysalis.mixin.misc;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
 import net.minecraft.ChatFormatting;
+import net.minecraft.commands.CommandBuildContext;
 import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.Commands;
+import net.minecraft.commands.arguments.EntityArgument;
+import net.minecraft.commands.arguments.ResourceArgument;
 import net.minecraft.core.Holder;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.commands.EnchantCommand;
 import net.minecraft.world.entity.Entity;
@@ -12,9 +19,11 @@ import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.sydokiddo.chrysalis.util.technical.commands.util.CommandCommonMethods;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import java.util.Collection;
 
@@ -52,5 +61,37 @@ public class EnchantCommandMixin {
                 cir.setReturnValue(0);
             }
         }
+    }
+
+    /**
+     * Reworks the /enchant command to allow for 'max' to be inserted in place of the level, which enchants the item with the maximum level of the given enchantment.
+     **/
+
+    @Unique
+    private static final String
+        chrysalis$targetsString = "targets",
+        chrysalis$enchantmentString = "enchantment",
+        chrysalis$levelString = "level"
+    ;
+
+    @Inject(method = "register", at = @At(value = "HEAD"), cancellable = true)
+    private static void chrysalis$changeEnchantCommand(CommandDispatcher<CommandSourceStack> commandDispatcher, CommandBuildContext commandBuildContext, CallbackInfo info) {
+
+        info.cancel();
+
+        commandDispatcher.register(Commands.literal("enchant").requires((commandSourceStack) -> commandSourceStack.hasPermission(2)).then(Commands.argument(chrysalis$targetsString, EntityArgument.entities())
+
+            .then((Commands.argument(chrysalis$enchantmentString, ResourceArgument.resource(commandBuildContext, Registries.ENCHANTMENT))
+                .executes((commandContext) -> EnchantCommand.enchant(commandContext.getSource(), EntityArgument.getEntities(commandContext, chrysalis$targetsString), ResourceArgument.getEnchantment(commandContext, chrysalis$enchantmentString), 1))
+            )
+
+            .then(Commands.argument(chrysalis$levelString, IntegerArgumentType.integer(0))
+                .executes((commandContext) -> EnchantCommand.enchant(commandContext.getSource(), EntityArgument.getEntities(commandContext, chrysalis$targetsString), ResourceArgument.getEnchantment(commandContext, chrysalis$enchantmentString), IntegerArgumentType.getInteger(commandContext, chrysalis$levelString)))
+            )
+
+            .then(Commands.literal("max")
+                .executes((commandContext) -> EnchantCommand.enchant(commandContext.getSource(), EntityArgument.getEntities(commandContext, chrysalis$targetsString), ResourceArgument.getEnchantment(commandContext, chrysalis$enchantmentString), ResourceArgument.getEnchantment(commandContext, chrysalis$enchantmentString).value().getMaxLevel()))
+            ))
+        ));
     }
 }
